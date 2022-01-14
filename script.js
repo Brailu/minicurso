7a0175c23610867f97cf36e01cb12e5c29882b8e
Brailu <ailon.antonio1@gmail.com>
2022-01-14 16:56:38 -0300

    Adicionar as capturas das mudanças html manipulaveis no java

---
+++ script.js
@@ -0,0 +1,5 @@
+const imagem = document.querySelector('img');
+const button = document.querySelector('button');
+const name_character = document.querySelector('#name');
+const species = document.querySelector('#species');
+const is_alive = document.querySelector('#is_alive');
