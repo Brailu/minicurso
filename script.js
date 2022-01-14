b7eb6d9ddd9887580cedabfc24e65de6ef276918
Brailu <ailon.antonio1@gmail.com>
2022-01-14 17:46:19 -0300

    Correção html, adicionar topicos no css e criação de api no JS

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 const imagem = document.querySelector('img');
-const button = document.querySelector('button');
+const button = document.querySelector('#button');
 const name_character = document.querySelector('#name');
 const species = document.querySelector('#species');
 const is_alive = document.querySelector('#is_alive');
+
+
+get_character = () => {
+    return fetch(`https://rickandmortyapi.com/api/character/2`, {
+        method:'GET', 
+        headers: {
+            Accpet: 'application/json',
+            "Content-type": 'application/json'
+        }
+    }).then((response) => response.json()).then ((data) => {
+        imagem.src = data.image; 
+        imagem.alt = data.name;
+        name_character.innerHTML = data.name;
+        species.innerHTML = data.species;
+        is_alive.innerHTML = data.status;
+
+    });
+}
+
+button.onclick = get_character;
+
